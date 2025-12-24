bc33a904fb5b7921b038a51f3c78def8477265b5
Sajawal149 <pfcdefender.com>
2025-12-24 16:47:37 +0500

    added new backgound

--- weatherapp-js/script.js
+++ weatherapp-js/script.js
@@ -1,3 +1,5 @@
 const input = document.getElementById('cityInput');
 const button = document.getElementById('getWeatherBtn');
 const resultDiv = document.getElementById('weatherResult');
+const api  = '2c8bf1ffa9d12294f473e9303fd223eb';
+
