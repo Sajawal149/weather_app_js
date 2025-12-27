9a972ad6f985a26e09e1decfb3ac864a1ff8eba8
Sajawal149 <pfcdefender.com>
2025-12-27 20:35:07 +0500

    code some javascript async function

--- weatherapp-js/script.js
+++ weatherapp-js/script.js
@@ -1,11 +1,15 @@
 const input = document.getElementById('cityInput');
 const button = document.getElementById('getWeatherBtn');
 const resultDiv = document.getElementById('weatherResult');
-// const api  = '2c8bf1ffa9d12294f473e9303fd223eb';
+const apikey = '2c8bf1ffa9d12294f473e9303fd223eb';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={API key}';
 
 async function checkWeather(city) {
     try {
-        const = responce.value(city);
+        const response = await fetch(url + city + `&appid{apikey}`);
+        var data = await response.json();
+
+        
         
     }
 }
