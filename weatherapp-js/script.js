6912256fad689afac94c1097c71cd3b122dd5635
Sajawal149 <pfcdefender.com>
2025-12-27 20:12:14 +0500

    updated css

--- weatherapp-js/script.js
+++ weatherapp-js/script.js
@@ -1,5 +1,12 @@
 const input = document.getElementById('cityInput');
 const button = document.getElementById('getWeatherBtn');
 const resultDiv = document.getElementById('weatherResult');
-const api  = '2c8bf1ffa9d12294f473e9303fd223eb';
+// const api  = '2c8bf1ffa9d12294f473e9303fd223eb';
+
+async function checkWeather(city) {
+    try {
+        const = responce.value(city);
+        
+    }
+}
 
